a67d0787f5ff1d8038adb8b30f00d59c0da7b724
diego Barrios <diegoarielbarriosmichalik@gmail.com>
2016-10-11 15:40:12 -0300

    bugs

--- app/src/test/java/com/dam/profesor/serviciosweb/ExampleUnitTest.java
+++ app/src/test/java/com/dam/profesor/serviciosweb/ExampleUnitTest.java
@@ -1,13 +1,11 @@
-package dev.ck.android_4kcomercios;
+package com.dam.profesor.serviciosweb;
 
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
  */
 public class ExampleUnitTest {
     @Test
